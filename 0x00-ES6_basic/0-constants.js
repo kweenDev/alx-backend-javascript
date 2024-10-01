34a2f46d339289d1ce760d3c8af910be23954aba
KweenDev <refiloe15radebe@gmail.com>
2024-10-01 06:57:01 +0200

    Added task for const and let demonstration (taskFirst and taskNext)

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+// 0-constants.js
+/**
+ * Task 0: Demonstrates the use of const and let for variable declaration.
+ * @returns {string} task - The task string.
+ * @returns {string} combination - The combination string modified by let.
+ */
+
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += ' is okay';
+    return combination;
+  }
