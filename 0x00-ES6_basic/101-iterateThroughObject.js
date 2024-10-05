e989d50f4e0715f7dba87c84f8b4371ff39d2014
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 06:02:53 +0200

    Add second advanced task

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,12 @@
+/**
+ * Iterates through the reportWithIterator and returns a string of employee names separated by '|'.
+ * @param {IterableIterator<string>} reportWithIterator - Iterator for employee names.
+ * @returns {string} - A string of employee names separated by '|'.
+ */
+export default function iterateThroughObject(reportWithIterator) {
+    const employeesArray = [];
+    for (const employee of reportWithIterator) {
+        employeesArray.push(employee);
+    }
+    return employeesArray.join(' | ');
+}
