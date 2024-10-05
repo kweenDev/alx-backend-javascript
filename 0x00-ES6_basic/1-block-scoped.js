bc4eef4c25c68c5b0385fa8d54838be0320a39a8
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 06:15:44 +0200

    Implemented block-scoped variables using let and const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,1 +1,12 @@
-//
+/* eslint-disable */
+export default function taskBlock(trueorFalse) {
+    const task = false;
+    const task2 = true;
+
+    if (trueorFalse) {
+        const task = true;
+        const task2 = false;
+    }
+
+    return [task, task2];
+}
