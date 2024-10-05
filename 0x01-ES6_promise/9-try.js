b58c2c1378e27221f0d02319aa8b1adf8ba6e0d4
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 08:43:46 +0200

    Create guardrail function to process math function and append result or error to queue and fix lint issues

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,9 @@
 /**
  * Executes a math function and appends the result or error message to a queue.
- * @param {Function} mathFunction - A function that performs a mathematical operation.
- * @returns {Array} An array containing the result of the math function and the string 'Guardrail was processed'.
+ * @param {Function} mathFunction - A function that performs a mathematical
+ * operation.
+ * @returns {Array} An array containing the result of the math function and
+ * the string 'Guardrail was processed'.
  */
 export default function guardrail(mathFunction) {
   const queue = [];
