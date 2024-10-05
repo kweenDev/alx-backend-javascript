807e6e2366cf0e69ceec3c33af1615a81a8b7d3d
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 08:29:17 +0200

    Fix lint issues

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 /**
  * Handles the response from a given Promise.
  * @param {Promise} promise - A Promise to handle.
- * @returns {Promise<Object>} A Promise that resolves to an object with status 200 and body 'success',
- * or an empty Error object if rejected. Logs "Got a response from the API" on every resolution.
+ * @returns {Promise<Object>} Promise that resolves to an object with status 200 and body
+ * 'success', or an empty Error object if rejected.
+ * Logs "Got a response from the API" on every resolution.
  */
 export default function handleResponseFromAPI(promise) {
   return promise
