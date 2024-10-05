88d52a9947399aae997ca76bffab707fd3ad9dd0
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 08:37:20 +0200

    Add signUpUser function to return resolved promise with user details and fix lint issues

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -2,7 +2,8 @@
  * Signs up a user and returns a resolved promise with the user's details.
  * @param {string} firstName - The first name of the user.
  * @param {string} lastName - The last name of the user.
- * @returns {Promise<Object>} A promise that resolves with an object containing the first and last name.
+ * @returns {Promise<Object>} A promise that resolves with an object containing
+ * the first and last name.
  */
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
