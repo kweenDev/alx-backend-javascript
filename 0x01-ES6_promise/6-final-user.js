12dbfa201843f0136a135f1cfa2cccd7ea3268c6
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 08:55:09 +0200

    Create handleProfileSignup function to manage multiple promises and return results with statuses and fix lint issues

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,27 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 /**
  * Handles the profile signup process, calling both signUpUser and uploadPhoto.
  * @param {string} firstName - The user's first name.
  * @param {string} lastName - The user's last name.
  * @param {string} fileName - The name of the file to upload.
- * @returns {Promise<Object[]>} A promise that resolves with an array of objects describing the status and value of each promise.
+ * @returns {Promise<Object[]>} A promise that resolves with an array of
+ * objects describing the status and value of each promise.
  */
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(firstName, lastName);
-  const upload = uploadPhoto(fileName);
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUp, upload])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
 }
