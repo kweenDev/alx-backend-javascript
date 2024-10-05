a73bd19cc2e6f48a1e02605a3053b72afae21dbf
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 06:01:44 +0200

    Add advanced task

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+/**
+ * Creates an iterator for employees in the report object.
+ * @param {Object} report - The report object created by createReportObject.
+ * @returns {IterableIterator<string>} - Iterator to go through each employee.
+ */
+export default function createIteratorObject(report) {
+    let employees = [];
+    for (const department of Object.values(report.allEmployees)) {
+        employees = employees.concat(department);
+    }
+    return employees[Symbol.iterator]();
+}
