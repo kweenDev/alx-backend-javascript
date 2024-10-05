3aa1c7ab5e48dddd8146fcd892d83ce3ee18b4dd
KweenDev <refiloe15radebe@gmail.com>
2024-10-05 08:39:37 +0200

    Implement uploadPhoto function to return rejected promise with error message and fix lint issues

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,8 @@
 /**
  * Uploads a photo and returns a rejected promise.
  * @param {string} fileName - The name of the file to upload.
- * @returns {Promise<Error>} A promise that rejects with an error indicating the file cannot be processed.
+ * @returns {Promise<Error>} A promise that rejects with an error indicating the
+ * file cannot be processed.
  */
 export default function uploadPhoto(fileName) {
   return Promise.reject(new Error(`${fileName} cannot be processed`));
