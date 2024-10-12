2a1b777f097d7a7074c0b634d9c415fb68fb4cb1
KweenDev <refiloe15radebe@gmail.com>
2024-10-12 02:22:59 +0200

    feat: add query tracking with WeakMap and error for high API endpoint load

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -8,15 +8,11 @@ export const weakMap = new WeakMap();
  * It should accept an endpoint argument
  * @param {*} endpoint - The endpoint to be queried
  */
+// https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
 export function queryAPI(endpoint) {
+  let called = 0;
   // Check how many times the endpoint has been queried
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    const count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, count + 1);
-  }
+  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
+  weakMap.set(endpoint, called + 1);
+  if (called + 1 >= 5) throw new Error('Endpoint load is high');
 }
