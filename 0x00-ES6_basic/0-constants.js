776ead802fd0d85ee91ae62b3868f0db9b790f42
KweenDev <refiloe15radebe@gmail.com>
2024-10-12 02:31:47 +0200

    Fix code structure and linting issues

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,10 +1,9 @@
 // 0-constants.js
 /**
-* Task 0: Demonstrates the use of const and let for variable declaration.
+* Demonstrates the use of const and let for variable declaration.
 * @returns {string} task - The task string.
 * @returns {string} combination - The combination string modified by let.
 */
-
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
