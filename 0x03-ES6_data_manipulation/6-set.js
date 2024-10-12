969073ae78d6ba980817c93147652676cd4d13af
KweenDev <refiloe15radebe@gmail.com>
2024-10-12 02:05:47 +0200

    feat: implement function to convert array into a Set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,12 @@
+// 6-set.js
+// Function to create a set from an array
+
+/**
+ * Function setFromArray
+ * Converts an array to a set
+ * @param {Array} array - Array to convert
+ * @returns {Set} - Set created from array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
