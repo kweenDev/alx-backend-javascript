1a576bed7d1ddc2c6f83168d2c321b54f8e84e9c
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 13:55:50 +0200

    feat: Extend Building class to create SkyHighBuilding with floors and evacuationWarningMessage

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,38 +1,16 @@
 import Building from './5-building';
 
-/**
- * SkyHighBuilding class that extends the Building class.
- *
- * @class SkyHighBuilding
- * @extends Building
- */
 export default class SkyHighBuilding extends Building {
-  /**
-     * Creates an instance of SkyHighBuilding.
-     *
-     * @param {Number} sqft - The size in square feet.
-     * @param {Number} floors - The number of floors.
-     */
   constructor(sqft, floors) {
-    super(sqft); // Calling parent class constructor
-    this._floors = floors; // Additional property specific to this class
+    super(sqft);
+    this._floors = floors;
   }
 
-  /**
-     * Getter for the number of floors.
-     *
-     * @returns {Number} The number of floors.
-     */
   get floors() {
     return this._floors;
   }
 
-  /**
-     * Override method for the evacuation warning message.
-     *
-     * @returns {String} Evacuation warning message.
-     */
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
