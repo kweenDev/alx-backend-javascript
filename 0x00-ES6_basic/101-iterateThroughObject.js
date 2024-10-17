afa417b00d0ae183b97cbcfbaa1516bcf8867289
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 09:25:17 +0200

    feat: Implement iterateThroughObject to format employee names as a string separated by pipes

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,6 @@
 /**
- * Iterates through the reportWithIterator and returns a string of employee names separated by '|'.
- * @param {IterableIterator<string>} reportWithIterator - Iterator for employee names.
- * @returns {string} - A string of employee names separated by '|'.
- */
+* Iterates through the reportWithIterator and returns a string of employee names separated by '|'.
+*/
 export default function iterateThroughObject(reportWithIterator) {
   const employeesArray = [];
   for (const employee of reportWithIterator) {
