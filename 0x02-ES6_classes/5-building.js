3c0470be631b304ae5bc6699eaab908680ee2142
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 13:53:40 +0200

    feat: Implement abstract Building class with sqft attribute

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,30 +1,18 @@
 /**
- * Represents a building.
- */
+* Represents a building.
+*/
 export default class Building {
-  /**
-     * Creates a new building.
-     * @param {number} sqft - The size of the building in square feet.
-     */
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+    if (typeof sqft !== 'number') throw TypeError('amount must be a number');
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
     this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  /**
-     * Abstract method for evacuation warning message.
-     * This must be implemented by any subclass of Building.
-     */
-  evacuationWarningMessage() {
-    if (this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 }
