15c249582b29a98873b3d63c8c99c6b77913bf6a
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 13:38:15 +0200

    Refactor: Use shorthand syntax for object property names in getBudgetObject in 7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -2,5 +2,11 @@
 * Returns a budget object with shorthand property names.
 */
 export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
 }
