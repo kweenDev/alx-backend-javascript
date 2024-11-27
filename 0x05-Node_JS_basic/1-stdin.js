6c29a9159b687f57537b34464944e8c49789f1a2
KweenDev <refiloe15radebe@gmail.com>
2024-11-27 03:37:30 +0200

    Task 1: Created '1-stdin.js' to prompt user for input via command line. Displays welcome message, captures user input, and handles exit message when program ends. Ensured compatibility with child process testing.

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    process.stdout.write('Your name is: ${input}\n');
+    process.exit();
+});
+
+process.on('exit', () => {
+    console.log("This important software is now closing");
+});
