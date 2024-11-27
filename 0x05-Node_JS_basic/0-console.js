170d675e8bd8124eb3a5c1afee1cd71f62b77c9a
KweenDev <refiloe15radebe@gmail.com>
2024-11-27 03:31:44 +0200

    Task 0: Implemented 'displayMessage' function to print a string to STDOUT. Tested functionality with example input 'Hello NodeJS!' as per project requirements.

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * displayMessage - Logs a message to the console
+ * @param {string} message - The message to log
+ */
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
