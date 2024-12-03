31bcb88bd184bb4bd7878ef3836a2f8fbb0e9191
KweenDev <refiloe15radebe@gmail.com>
2024-12-03 18:04:52 +0200

    Add 5-payment.js and test suite with hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -3,11 +3,24 @@ const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./5-payment');
 const Utils = require('./utils');
 
-describe('sendPaymentRequestToApi', function () {
-    it('should use Utils.calculateNumber', function () {
-      const spy = sinon.spy(Utils, 'calculateNumber');
+describe('sendPaymentRequestToApi', () => {
+    let consoleSpy;
+
+    beforeEach(() => {
+      consoleSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+      consoleSpy.restore();
+    });
+
+    it('logs the correct total for 100 and 20', () => {
       sendPaymentRequestToApi(100, 20);
-      sinon.assert.calledWith(spy, 'SUM', 100, 20);
-      spy.restore();
+      sinon.assert.calledOnceWithExactly(consoleSpy, 'The total is: 120');
+    });
+
+    it('logs the correct total for 10 and 10', () => {
+      sendPaymentRequestToApi(10, 10);
+      sinon.assert.calledOnceWithExactly(consoleSpy, 'The total is: 20');
     });
 });
