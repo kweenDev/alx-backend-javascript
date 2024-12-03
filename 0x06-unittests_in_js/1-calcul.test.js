3a5943a682ed7c9267811c277e168aed65e4bdf7
KweenDev <refiloe15radebe@gmail.com>
2024-12-03 16:49:19 +0200

    feat: add support for multiple operations in calculateNumber with tests

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,27 @@
+// 1-calcul.test.js
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+    describe('SUM', () => {
+        it('should return 6 when inputs are 1.4 and 4.5', () => {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
+    });
+
+    describe('SUBTRACT', () => {
+        it('should return -4 when inputs are 1.4 and 4.5', () => {
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+    });
+
+    describe('DIVIDE', () => {
+        it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        });
+
+        it('should return Error when divisor is 0', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        });
+    });
+});
