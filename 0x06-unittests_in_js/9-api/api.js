8ff0503984ff9e67821254cdc8bf9fe938ec7a17
KweenDev <refiloe15radebe@gmail.com>
2024-12-06 13:40:02 +0200

    Add route validation for cart/:id and tests for numeric and non-numeric ids

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,15 +5,12 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-    const cartId = req.params.id;
-    if (/^\d+$/.test(cartId)) {
-        res.send('Payment methods for cart ${cartId}');
-    } else {
-        res.status(404).send('Not found');
-    }
-});
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+  });
 
-app.listen(7865, () => {
+  app.listen(7865, () => {
     console.log('API available on localhost port 7865');
-});
+  });
+
+module.exports = app;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,18 +1,33 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Cart page', () => {
-  it('Correct status code when :id is a number?', (done) => {
-    request.get('http://localhost:7865/cart/12', (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
+describe('Cart page', function () {
+  it('tests the server', function (done) {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should return status code 200 when id is a number', function (done) {
+    request.get('http://localhost:7865/cart/12', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Payment methods for cart 12');
       done();
     });
   });
 
-  it('Correct status code when :id is NOT a number?', (done) => {
-    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
-      expect(res.statusCode).to.equal(404);
+  it('should return status code 404 when id is not a number', function (done) {
+    request.get('http://localhost:7865/cart/hello', function (error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('tests the server with wrong cart id', function (done) {
+    request.get('http://localhost:7865/cart/wrong', function (error, response, body) {
+      expect(response.statusCode).to.equal(404);
       done();
     });
   });
