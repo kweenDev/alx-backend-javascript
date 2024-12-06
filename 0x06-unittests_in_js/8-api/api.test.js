eaadc252b202a841cfaad1ffd6bab7eb43f612cb
KweenDev <refiloe15radebe@gmail.com>
2024-12-06 13:21:12 +0200

    Set up basic Express server with index route and test suite for basic integration

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+    it('Correct status code?', (done) => {
+        request.get('http://localhost:7865', (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Correct result?', (done) => {
+        request.get('http://localhost:7865', (err, res, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+});
